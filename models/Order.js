6e0a1f7ce0a8668d873b533e11774d141e56e6d4
unknown <lautarorangilt@gmail.com>
2022-02-06 18:18:53 -0300

    new project

---
+++ models/Order.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const   OrderSchema = new mongoose.Schema(
+    {
+        userId: { type: String, required: true },
+        products: [
+            {
+                productId: {
+                type:String
+            },
+                quantity: {
+                    type: Number,
+                    default: 1,
+                }
+
+        }
+        ],
+        ammount: {type: Number, required:true},
+        adress:{type: Object, required: true},
+        status: {trpe:String, default: "pending"},
+    
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", OrderSchema)
---
+++ routes/user.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+
+module.exports = router
+
+
+// router.get("/usertest", (req, res) => {
+//     res.send("user test is successfull");
+// })
+
+// router.post("/userposttest", (req, res) => {
+//     const username = req.body.username
+//     res.send("yout user name is : " + username);
+// } )
